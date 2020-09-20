3c7c813513a8031ddcc433a0949fd576771f9628
Dpavaman <67939138+Dpavaman@users.noreply.github.com>
2020-09-20 19:59:54 +0530

    fixed Bugs

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ async function addStudent() {
         mentorAssigned: false,
         mentorName: "Not Assigned"
     };
-    await fetch("https://student-mentor-assign-backend.herokuapp.com//student", {
+    await fetch("https://student-mentor-assign-backend.herokuapp.com/student", {
         method: "POST",
         body: JSON.stringify(data),
         headers: {
@@ -37,7 +37,7 @@ async function addMentor() {
         email: document.getElementById("mentorEmail").value,
         students: []
     }
-    await fetch("https://student-mentor-assign-backend.herokuapp.com//mentor", {
+    await fetch("https://student-mentor-assign-backend.herokuapp.com/mentor", {
         method: "POST",
         body: JSON.stringify(data),
         headers: {
@@ -53,7 +53,7 @@ async function assignStudents() {
         mentor: selectedMentor,
         studentName: assignThisStudent
     }
-    await fetch('https://student-mentor-assign-backend.herokuapp.com//mentor/addStudent', {
+    await fetch('https://student-mentor-assign-backend.herokuapp.com/mentor/addStudent', {
         method: 'PUT',
         body: JSON.stringify(data),
         headers: {
@@ -63,7 +63,7 @@ async function assignStudents() {
 }
 
 async function fetchMentors() {
-    let response = await fetch("https://student-mentor-assign-backend.herokuapp.com//mentor");
+    let response = await fetch("https://student-mentor-assign-backend.herokuapp.com/mentor");
     let list = await response.json();
     list.forEach(element => {
         mentors.push(element)
@@ -71,7 +71,7 @@ async function fetchMentors() {
 }
 
 async function fetchStudents() {
-    let response = await fetch("https://student-mentor-assign-backend.herokuapp.com//student");
+    let response = await fetch("https://student-mentor-assign-backend.herokuapp.com/student");
     let list = await response.json();
     list.forEach(element => {
         students.push(element);
@@ -236,37 +236,3 @@ function displayMentorForm() {
     document.getElementById('studentForm').style.display = 'none';
     document.getElementById('chooseField').style.display = 'none';
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-mentors.forEach(element => {
-
-    let card = document.createElement('div');
-            card.classList.add('card', 'bg-transparent', "col");
-            card.innerHTML = ` <h1> <u> ${element.name} </u> </h1> <br>`;
-
-
-    let ol = document.createElement('ol');
-        (element.studentList).forEach(item => {
-            let li = document.createElement('li');
-            li.innerHTML = `${item}`;
-        });
-        
-    });
